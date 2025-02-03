ba8941dbd2c5b071ce684a4331d6865b52b46523
Gomez Martin <mgome536@edu.gencat.cat>
2025-02-03 12:27:15 +0100

    Correcció del port per a Railway

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 if __name__ == '__main__':
     import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=False, host='0.0.0.0', port=5000)
+app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
 
